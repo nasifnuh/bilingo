6f37f70fe5b001515cd7f4ddedd4e290d47ec378
Nasif Nuha <nasifnuh@gmail.com>
2025-02-04 18:42:56 -0500

    [add] setup path aliases

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@assets": "./assets",
+            "@": "./src",
+            "@components": "./src/components",
+            "@context": "./src/context",
+            "@layout": "./src/layout",
+            "@navigation": "./src/navigation",
+            "@redux": "./src/redux",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@translations": "./src/translations",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
+  };
+};
