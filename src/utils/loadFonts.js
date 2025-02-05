1c8dd8e97a26d70f7153a238cf1f45dc508a48f2
Nasif Nuha <nasifnuh@gmail.com>
2025-02-05 00:40:21 -0500

    [fix] rename font alias

--- src/utils/loadFonts.js
+++ src/utils/loadFonts.js
@@ -2,10 +2,10 @@ import * as Font from "expo-font";
 
 export const loadFonts = async () => {
   await Font.loadAsync({
-    "BalooChettanR": require("../../assets/fonts/BalooChettan2-Regular.ttf"), // 400
-    "BalooChettanM": require("../../assets/fonts/BalooChettan2-Medium.ttf"), // 500
-    "BalooChettanSB": require("../../assets/fonts/BalooChettan2-SemiBold.ttf"), // 600
-    "BalooChettanB": require("../../assets/fonts/BalooChettan2-Bold.ttf"), // 700
-    "BalooChettanEB": require("../../assets/fonts/BalooChettan2-ExtraBold.ttf"), // 800
+    "BalooChettan-R": require("../../assets/fonts/BalooChettan2-Regular.ttf"), // 400
+    "BalooChettan-M": require("../../assets/fonts/BalooChettan2-Medium.ttf"), // 500
+    "BalooChettan-SB": require("../../assets/fonts/BalooChettan2-SemiBold.ttf"), // 600
+    "BalooChettan-B": require("../../assets/fonts/BalooChettan2-Bold.ttf"), // 700
+    "BalooChettan-EB": require("../../assets/fonts/BalooChettan2-ExtraBold.ttf"), // 800
   });
 };
