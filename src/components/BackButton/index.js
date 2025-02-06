71c4ab8dfc5f5a757a1585c3d8cf53361b5a0285
Nasif Nuha <nasifnuh@gmail.com>
2025-02-06 00:15:16 -0500

    [feat] reusable back button component

---
+++ src/components/BackButton/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Octicons } from "@expo/vector-icons";
+
+import colors from "@constants/colors";
+
+const BackButton = ({ screenName }) => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    if (screenName) {
+      navigation.navigate(screenName);
+    } else {
+      navigation.goBack();
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Octicons name="chevron-left" size={24} color={colors.darkGray} />
+    </TouchableOpacity>
+  );
+};
+
+export default BackButton;
