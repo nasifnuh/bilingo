e5cea4bbc0b389755188e7d4115f9c253e4f16cd
Nasif Nuha <nasifnuh@gmail.com>
2025-02-08 17:50:27 -0500

    #4 Start implementing creating user profile

--- src/screens/Signup/index.js
+++ src/screens/Signup/index.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image } from "react-native";
 
-const Signup = () => (
-  <View style={styles.container}>
-    <Text>Signup Screen</Text>
-  </View>
-);
+import Layout from "@/layout";
+import BackButton from "@components/BackButton";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-});
+import MascotImage from "@assets/images/mascot_love.png";
+import { styles } from "./styles";
+
+const Signup = () => {
+
+  return (
+    <Layout>
+      <View style={styles.container}>
+        <BackButton />
+        <Image source={MascotImage} style={styles.image} />
+        <Text style={styles.title}>Create an Account</Text>
+      </View>
+    </Layout>
+  );
+};
 
 export default Signup;
