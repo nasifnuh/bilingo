b314bc606b785d6d1963a929306816b425a62458
Nasif Nuha <nasifnuh@gmail.com>
2025-02-08 16:17:45 -0500

    [feat] bottom tab navigation

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,20 +1,73 @@
 import React from "react";
+import { View, Image, Platform, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 // import HomeScreen from "@screens/HomeScreen";
 import HomeStackNavigator from "@navigation/stacks/HomeStackNavigator";
-import FavoritesScreen from "@screens/FavoritesScreen";
+import Favorite from "@screens/Favorite";
+import Stats from "@screens/Stats";
+import Profile from "@screens/Profile";
+
+import Colors from "@constants/colors";
+
+const icons = {
+  home: require("../../assets/images/icons/home.png"),
+  favorites: require("../../assets/images/icons/heart.png"),
+  stats: require("../../assets/images/icons/pie.png"),
+  profile: require("../../assets/images/icons/man.png"),
+};
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarLabel: () => null,
+      tabBarStyle: {
+        height: Platform.OS === "ios" ? 85 : 54,
+        paddingTop: 5,
+        borderTopWidth: 3,
+      },
+      tabBarActiveTintColor: "transparent",
+      tabBarInactiveTintColor: "transparent",
+      tabBarIcon: ({ focused }) => {
+        const icon = icons[route.name.toLowerCase()];
+        return (
+          <View
+            style={[styles.tabBarIcon, focused && styles.tabBarIconFocused]}
+          >
+            <Image source={icon} style={styles.icon} />
+          </View>
+        );
+      },
+    })}
+  >
     {/* Keeps the bottom tab navigation visible even when navigating deep within HomeStack.
     Remove this if it is not needed and delete the files under @navigation/stacks */}
-    <Tab.Screen name="HomeStack" component={HomeStackNavigator} />
+    <Tab.Screen name="Home" component={HomeStackNavigator} />
     {/* <Tab.Screen name="HomeStack" component={HomeScreen} /> */}
 
-    <Tab.Screen name="FavoritesStack" component={FavoritesScreen} />
+    <Tab.Screen name="Favorites" component={Favorite} />
+    <Tab.Screen name="Stats" component={Stats} />
+    <Tab.Screen name="Profile" component={Profile} />
   </Tab.Navigator>
 );
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  tabBarIcon: {
+    borderRadius: 8,
+    padding: 4,
+  },
+  tabBarIconFocused: {
+    borderWidth: 1,
+    backgroundColor: Colors.pastelPurple,
+    borderColor: Colors.royalPurple,
+  },
+});
 
 export default BottomTabNavigator;
