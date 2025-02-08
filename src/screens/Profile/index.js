d4715a0e429381233951d96a8295c0fb0079aa6e
Nasif Nuha <nasifnuh@gmail.com>
2025-02-08 18:41:18 -0500

    #4 [feat] UI for the profile screen

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,19 +1,90 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-const Profile = () => (
-  <View style={styles.container}>
-    <Text>Profile Screen</Text>
-  </View>
-);
+import Layout from "@/layout";
+import Button from "@components/Button";
+import TextInput from "@components/TextInput";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
+import { styles } from "./styles";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
 });
+
+const Profile = () => {
+  const [editing, setEditing] = useState(false);
+
+  return (
+    <Layout headerComponent={<Text style={styles.headerLabel}>Profile</Text>}>
+      <View style={styles.container}>
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            password: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {}}
+        >
+          {({
+            handleChange,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+            isSubmitting,
+          }) => (
+            <View style={styles.form}>
+              <TextInput
+                label="Name"
+                onChangeText={handleChange("name")}
+                value={values.name}
+                placeholder="Enter your name"
+                error={errors.name && touched.name}
+                disabled={!editing || isSubmitting}
+              />
+              <TextInput
+                label="Email"
+                onChangeText={handleChange("email")}
+                value={values.email}
+                placeholder="Enter your email"
+                keyboardType="email-address"
+                error={errors.email && touched.email}
+                disabled={!editing || isSubmitting}
+              />
+              <TextInput
+                label="Password"
+                onChangeText={handleChange("password")}
+                value={values.password}
+                placeholder="Enter your password"
+                secureTextEntry={true}
+                error={errors.password && touched.password}
+                disabled={!editing || isSubmitting}
+              />
+
+              <View>
+                <Button
+                  label={editing ? "Update Profile" : "Edit Profile"}
+                  onPress={() => (editing ? handleSubmit() : setEditing(true))}
+                  loading={isSubmitting}
+                  style={styles.editButton}
+                />
+                <Button
+                  label="Delete account"
+                  variant="outlined"
+                  onPress={() => console.log("Handle Delete")}
+                />
+              </View>
+            </View>
+          )}
+        </Formik>
+      </View>
+    </Layout>
+  );
+};
 
 export default Profile;
---
+++ src/screens/Profile/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+import Colors from "@constants/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 18,
+    flex: 1,
+  },
+  headerLabel: {
+    textAlign: "center",
+    fontFamily: "BalooChettan-B",
+    fontSize: 18,
+    color: Colors.black,
+  },
+  form: {
+    gap: 24,
+  },
+  editButton: {
+    marginTop: 40,
+    marginBottom: 10,
+  },
+});
