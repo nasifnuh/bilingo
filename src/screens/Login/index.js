e24c203177c88f6f0aaf22ab4e4a70d51c92df3b
Nasif Nuha <nasifnuh@gmail.com>
2025-02-08 18:21:28 -0500

    #4 Login screen UI

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,19 +1,78 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-const Login = () => (
-  <View style={styles.container}>
-    <Text>Login Screen</Text>
-  </View>
-);
+import Layout from "@/layout";
+import BackButton from "@components/BackButton";
+import Button from "@components/Button";
+import TextInput from "@components/TextInput";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
+import MascotImage from "@assets/images/mascot_excited.png";
+
+import { styles } from "./styles";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
 });
+
+const Login = () => {
+  return (
+    <Layout>
+      <View style={styles.container}>
+        <BackButton style={styles.backButton} />
+        <Image source={MascotImage} style={styles.image} />
+        <Text style={styles.title}>Welcome Back!</Text>
+
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {}}
+        >
+          {({
+            handleChange,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+            isSubmitting,
+          }) => (
+            <View style={styles.form}>
+              <TextInput
+                label="Email"
+                onChangeText={handleChange("email")}
+                value={values.email}
+                placeholder="Enter your email"
+                keyboardType="email-address"
+                error={errors.email && touched.email}
+                disabled={isSubmitting}
+              />
+              <TextInput
+                label="Password"
+                onChangeText={handleChange("password")}
+                value={values.password}
+                placeholder="Enter your password"
+                secureTextEntry={true}
+                error={errors.password && touched.password}
+                disabled={isSubmitting}
+              />
+
+              <Button
+                label="Login"
+                onPress={handleSubmit}
+                loading={isSubmitting}
+                style={styles.loginButton}
+              />
+            </View>
+          )}
+        </Formik>
+      </View>
+    </Layout>
+  );
+};
 
 export default Login;
