a9a18639118d1599099e73e4fdf9d696a2a3cd91
Nasif Nuha <nasifnuh@gmail.com>
2025-03-01 13:40:20 -0500

    #5 [fix] install notification related packages

--- src/components/CourseUnit/LessonCard/index.js
+++ src/components/CourseUnit/LessonCard/index.js
@@ -1,35 +1,11 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { useNavigation, useFocusEffect } from "@react-navigation/native";
-import { FontAwesome6 } from "@expo/vector-icons";
-
-import { ref, get } from "firebase/database";
-import { auth, database } from "@services/firebaseConfig";
-
-import Colors from "@constants/colors";
+import { useNavigation } from "@react-navigation/native";
 
 import { styles } from "./styles";
 
-const LessonCard = ({ lessons, color }) => {
+const LessonCard = ({ lessons }) => {
   const navigation = useNavigation();
-  const [progress, setProgress] = useState([]);
-
-  useFocusEffect(
-    useCallback(() => {
-      const fetchProgress = async () => {
-        const user = auth.currentUser;
-        if (user) {
-          const userId = user.uid;
-          const progressRef = ref(database, `users/${userId}/progress`);
-          const snapshot = await get(progressRef);
-          const currentProgress = snapshot.val() || [];
-          setProgress(currentProgress);
-        }
-      };
-
-      fetchProgress();
-    }, [])
-  );
 
   return (
     <View style={styles.container}>
@@ -39,32 +15,11 @@ const LessonCard = ({ lessons, color }) => {
           style={styles.lessonContainer}
           onPress={() => navigation.navigate("Lesson", { lesson: lesson })}
         >
-          <View
-            style={[
-              styles.unit,
-              progress.includes(lesson.id) && { borderColor: color },
-            ]}
-          >
-            {progress.includes(lesson.id) ? (
-              <FontAwesome6 name="check" size={24} color={color} />
-            ) : (
-              <Text style={styles.unitLabel}>{lesson.unit}</Text>
-            )}
+          <View style={styles.unit}>
+            <Text style={styles.unitLabel}>{lesson.unit}</Text>
           </View>
-          <View
-            style={[
-              styles.lesson,
-              progress.includes(lesson.id) && { borderColor: color },
-            ]}
-          >
-            <Text
-              style={[
-                styles.lessonLabel,
-                progress.includes(lesson.id) && { color: color },
-              ]}
-            >
-              {lesson.name}
-            </Text>
+          <View style={styles.lesson}>
+            <Text style={styles.lessonLabel}>{lesson.name}</Text>
           </View>
         </TouchableOpacity>
       ))}
