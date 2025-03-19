266c17e00bf7462cd2f13a486dd0f647b274855f
Nasif Nuha <nasifnuh@gmail.com>
2025-03-18 23:33:52 -0400

    #16 custom ui text component

--- src/screens/Accessibility/index.js
+++ src/screens/Accessibility/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View } from "react-native";
 import Slider from "@react-native-community/slider";
 
+import { useFontSize } from "@/context/FontSizeContext";
+
 import Layout from "@/layout";
 import Text from "@/components/ui/Text";
 import BackButton from "@/components/BackButton";
@@ -9,6 +11,7 @@ import BackButton from "@/components/BackButton";
 import { styles } from "./styles";
 
 const Accessibility = () => {
+  const { scale, setScale } = useFontSize();
 
   return (
     <Layout
@@ -21,14 +24,14 @@ const Accessibility = () => {
     >
       <View style={styles.container}>
         <View style={styles.form}>
-          <Text style={styles.label}>Font Scale:</Text>
+          <Text style={styles.label}>Font Scale: {scale.toFixed(2)}</Text>
           <Slider
             style={styles.slider}
-            minimumValue={0.5}
-            maximumValue={2.0}
+            minimumValue={0.8}
+            maximumValue={1.5}
             step={0.1}
             value={scale}
-            onValueChange={(value) => console.log('valku: ', value)}
+            onValueChange={(value) => setScale(value)}
           />
         </View>
       </View>
