7676908f8efe88860f950c5f13defa50fb86de04
Lalana Chamika <lalanachamika123@gmail.com>
2025-03-22 15:58:00 -0400

    #9 - Initial model changes
    Adding the word of the day model changes

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,7 +1,42 @@
 import { StyleSheet } from "react-native";
+import Colors from "@constants/colors";
 
 export const styles = StyleSheet.create({
   container: {
     paddingVertical: 18,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  word: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  pronounce: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  phoneticTranscript: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  meaning: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  closeButtonText: {
+    fontSize: 18,
+    color: Colors.royalPurple,
   },
 });
