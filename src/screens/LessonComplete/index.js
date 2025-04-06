393d6e90928924a4cba18d75cc5af59e7567ea67
Lalana Chamika <lalanachamika123@gmail.com>
2025-04-06 09:48:50 -0400

    #12 - Applying dark theme changes to Lesson Complete screen
    Applying dark theme changes to Lesson Complete screen

--- src/screens/LessonComplete/index.js
+++ src/screens/LessonComplete/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, useColorScheme } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { FormattedMessage } from "react-intl";
 
@@ -14,29 +14,30 @@ import { styles } from "./styles";
 const LessonComplete = ({ route }) => {
   const navigation = useNavigation();
   const { isDaysFirstLesson, streak } = route.params;
+  const colorScheme = useColorScheme(); // Add color scheme detection
 
   return (
     <Layout>
       <View style={styles.container}>
-        <View style={styles.content}>
-          <Text style={styles.title}>
+        <View style={[styles.content, colorScheme === "dark" && styles.contentDark]}>
+          <Text style={[styles.title, colorScheme === "dark" && styles.titleDark]}>
             <FormattedMessage id="lessonCompleted" />!
           </Text>
           <Image source={MascotImage} style={styles.image} />
-          <View style={styles.rewardContainer}>
+          <View style={[styles.rewardContainer, colorScheme === "dark" && styles.rewardContainerDark]}>
             <View style={styles.rewardTitleContainer}>
-              <Text style={styles.rewardTitleContainerText}>
+              <Text style={[styles.rewardTitleContainerText, colorScheme === "dark" && styles.rewardTitleContainerTextDark]}>
                 <FormattedMessage id="gains" /> 💪
               </Text>
             </View>
             <View style={styles.rewardValueContainer}>
               <View style={{ display: "flex", flexDirection: "row", gap: 10 }}>
-                <Text style={styles.icon}>💎</Text>
-                <Text style={styles.reward}>+5</Text>
+                <Text style={[styles.icon, colorScheme === "dark" && styles.iconDark]}>💎</Text>
+                <Text style={[styles.reward, colorScheme === "dark" && styles.rewardDark]}>+5</Text>
               </View>
               <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
-                <Text style={styles.icon}>⚡️</Text>
-                <Text style={styles.reward}>+10</Text>
+                <Text style={[styles.icon, colorScheme === "dark" && styles.iconDark]}>⚡️</Text>
+                <Text style={[styles.reward, colorScheme === "dark" && styles.rewardDark]}>+10</Text>
               </View>
             </View>
           </View>
